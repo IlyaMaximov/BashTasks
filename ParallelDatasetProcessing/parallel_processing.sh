bf588ffe196c57134acbe2556426afdfd21372d5
Илья Максимов <IlyaMaximovv@yandex.ru>
2021-04-16 12:19:18 +0300

    Parallel processing task bag fix

--- ParallelDatasetProcessing/parallel_processing.sh
+++ ParallelDatasetProcessing/parallel_processing.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-if [[ $# < 6 || $1 != "--worker_cnt" || $3 != "--data_link" || $5 != "--dest_dir" ]]; then
+if [[ $# < 5 || $1 != "--worker_cnt" || $3 != "--dest_dir" ]]; then
 	echo "Incorrect format or count of arguments"
 	exit
 fi
 
 worker_cnt=$2
-links=$4
-dest_dir=$6
+dest_dir=$4
+links="tmp_links.txt"
+
+while [ -n "$5" ]
+do
+echo $5 >> $links
+shift
+done
 
 # $links = https://drive.google.com/uc?export=download&id=1EfRc2RLVdwWlXWz3nDIBEv_EvvOMd9ip
 parallel -a $links -d '\n' -j $worker_cnt wget -P $dest_dir {}
+
+rm $links 2>/dev/null 
